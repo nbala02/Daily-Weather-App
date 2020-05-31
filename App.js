ec09231df5e71554a00e66f0cdf2572d7afcd090
nbala02 <63129564+nbala02@users.noreply.github.com>
2020-05-30 23:48:52 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Forecast from "./Weather/Forecast"
+
+// Use the function app to return the weather application
+function App() {
+  return(
+    <div>
+      <Forecast />
+    </div>
+  )
+}
+
+export default App;
