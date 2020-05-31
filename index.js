572f4ae13e0f05b5726e8dc4aa57418c2c3dfd0d
nbala02 <63129564+nbala02@users.noreply.github.com>
2020-05-30 23:50:58 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from "./App"
+
+// Use React DOM to render the weather app and get the element by the id "root"
+ReactDOM.render(<App />, document.getElementById('root'))
